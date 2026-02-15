4284290adc6aadfb7bc1ae85ed436c207c46b5ee
iwant-zero <ysk37261971@gmail.com>
2026-02-15 20:58:05 +0900

    Update game.js

--- game.js
+++ game.js
@@ -10,25 +10,22 @@ window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
 // 플레이어
-const playerImg = new Image();
-playerImg.src = "assets/player_sheet.png";
-
 let player = { x: 100, y: 300, w: 64, h: 64, vy: 0, hp: 100 };
 
 // 몬스터
-const slimeImg = new Image();
-slimeImg.src = "assets/slime_sheet.png";
-
 let slimes = [
   { x: 400, y: 300, w: 64, h: 64, hp: 30 }
 ];
 
-// 효과음
-const hitSound = new Audio("assets/sfx_hit.mp3");
-
 let keys = {};
 document.addEventListener("keydown", e => keys[e.key] = true);
 document.addEventListener("keyup", e => keys[e.key] = false);
+
+function jump() {
+  if (player.vy === 0) {
+    player.vy = -12;
+  }
+}
 
 function update() {
   // 중력
@@ -42,7 +39,7 @@ function update() {
 
   if (keys["ArrowLeft"]) player.x -= 5;
   if (keys["ArrowRight"]) player.x += 5;
-  if (keys["ArrowUp"] && player.vy === 0) player.vy = -12;
+  if (keys["ArrowUp"]) jump();
 
   // 몬스터 충돌 체크
   slimes.forEach(slime => {
@@ -51,7 +48,6 @@ function update() {
         player.y < slime.y + slime.h &&
         player.y + player.h > slime.y) {
       slime.hp -= 1;
-      hitSound.play();
     }
   });
 
@@ -62,13 +58,13 @@ function update() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // 플레이어
-  ctx.drawImage(playerImg, player.x, player.y, player.w, player.h);
+  // 플레이어 (빨간 네모)
+  ctx.fillStyle = "red";
+  ctx.fillRect(player.x, player.y, player.w, player.h);
 
-  // 몬스터
-  slimes.forEach(slime => {
-    ctx.drawImage(slimeImg, slime.x, slime.y, slime.w, slime.h);
-  });
+  // 몬스터 (초록 네모)
+  ctx.fillStyle = "lime";
+  slimes.forEach(slime => ctx.fillRect(slime.x, slime.y, slime.w, slime.h));
 
   // HUD
   ctx.fillStyle = "white";
